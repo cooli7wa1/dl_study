a2c781f8aa2b6c4549619876935bd1ae45d20f91
Rickey <429118074@qq.com>
2018-02-03 19:17:22 +0800

    update fuzzing

---
+++ fuzzing/create_binary_file.py
@@ -0,0 +1,26 @@
+import os, re
+
+folder='E:\\PycharmProjects\\dl_study\\fuzzing\\data\\'
+ori_files=os.listdir(folder)
+target_files=[]
+for file in ori_files:
+    if re.match(r'^(fuzzing|success)[^\.]+$', file):
+        target_files.append(file)
+
+for file in target_files:
+    print('deal with %s ...'%file)
+    f = open(folder+file, 'r')
+    f1 = open(folder+'%s.dat'%file, 'wb')
+    for line in f:
+        list = [line[i:i+2] for i in range(0, len(line), 2)]
+        for str in list:
+            if str == '\n':
+                continue
+            hex = int(str, 16)
+            f1.write(hex.to_bytes(1, byteorder='big'))
+    f.close()
+    f1.close()
+
+# f1 = open(folder+'fuzzing_train_12000.dat', 'rb')
+# print(struct.unpack('i', f1.read()[:4]))
+# f1.close()
--- fuzzing/input_data.py
+++ fuzzing/input_data.py
@@ -6,8 +6,8 @@ def _read(filename_queue):
     class FuzzingRecord():
         pass
     result = FuzzingRecord()
-    label_bytes = 2
-    data_bytes = DATA_SIZE*2
+    label_bytes = LABEL_SIZE
+    data_bytes = DATA_SIZE
     record_bytes = label_bytes + data_bytes
     reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
     result.key, value = reader.read(filename_queue)
@@ -17,32 +17,39 @@ def _read(filename_queue):
     result.data = tf.cast(tf.strided_slice(record_bytes, [label_bytes],
                        [label_bytes + data_bytes]), tf.float32)
     
-    result.data = tf.reshape(result.data, [1,192])
+    result.data = tf.reshape(result.data, [1,DATA_SIZE])
     result.label.set_shape([1])
     return result
 
-def _generate_batch(data, label, min_queue_examples, batch_size):
-    pass
 
-def input(filenames, batch_size):
+def input(filenames):
     with tf.name_scope('input'):
         filename_queue = tf.train.string_input_producer(filenames)
         read_input = _read(filename_queue)
+
         data, label = tf.train.shuffle_batch([read_input.data, read_input.label],
-                                             batch_size=10,
-                                             capacity=30,
-                                             min_after_dequeue=20)
+                                             num_threads=16,
+                                             batch_size=BATCH_SIZE,
+                                             capacity=CAPACITY,
+                                             min_after_dequeue=MIN_AFTER_DEQUEUE)
     return data, label
 
-data, label = input(['/home/cooli7wa/study/tensorflow_data/fuzzing_data/clear_data_fuzzing'], 0)
-a = tf.Print(data, [data], message='data:')
 
-with tf.Session() as sess:
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-    data = sess.run(a)
-    print(np.shape(data))
-    print(data[0])
-    coord.request_stop()
-    coord.join(threads)
+def for_test():
+    folder = 'E:\\PycharmProjects\\dl_study\\fuzzing\\data\\'
+    # file_list = [folder+'success_train_12000.dat', folder+'fuzzing_train_12000.dat']
+    file_list = [folder+'mix_train_24000.dat']
+    data, label = input(file_list)
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        out_data, out_label = sess.run((data, label))
+        print(np.shape(data), np.shape(label))
+        for i in range(10):
+            print(out_label[i])
+        print(out_data[0])
+        coord.request_stop()
+        coord.join(threads)
 
+if __name__ == '__main__':
+    for_test()
