cb3599e39a73fcaa5aa077c0af162725e1c93fc9
Rickey <429118074@qq.com>
2018-02-25 13:04:53 +0800

    modify folder

---
+++ fuzzing/mine/new/data/gen_data.py
@@ -0,0 +1,75 @@
+import os
+import binascii
+import random
+
+FUZZING_FOLDER = 'E:\\PycharmProjects\\dl_study\\fuzzing\\hangyu\\data\\fuzzing'
+NORMAL_FOLDER = 'E:\\PycharmProjects\\dl_study\\fuzzing\\hangyu\\data\\normal'
+OUTPUT_FOLDER = 'E:\\PycharmProjects\\dl_study\\fuzzing\\hangyu\\data\\'
+PROPORTION = 2/3  # train_data/total_data
+LOG_FILE = OUTPUT_FOLDER + 'gen.log'
+
+log = []
+
+def parse_raw_data_to_bin_list(folder, label):
+    files = os.listdir(folder)
+    ascii_datas = []
+    for fuzzing_file in files:
+        with open(os.path.join(folder, fuzzing_file)) as f:
+            raw_lines = f.readlines()
+            for line in raw_lines:
+                line_list = line.replace('\n','').split(" ")
+                if len(line_list) == 3 and line_list[2] != '':
+                    ascii_datas.append(line_list[2])
+    non_repeat_ascii_datas = list(set(ascii_datas))
+    log.append("origin data number: %d" % len(ascii_datas))
+    log.append("non-repeat data number: %d" % len(non_repeat_ascii_datas))
+
+    bin_list = []
+    for ascii_data in non_repeat_ascii_datas:
+        ascii_data = label+ascii_data
+        bin_data = binascii.unhexlify(ascii_data)
+        bin_list.append(bin_data)
+    log.append('bin_list len %d' % len(bin_list))
+    log.append('bin_list[0] len %d' % len(bin_list[0]))
+    return bin_list
+
+def intercept_data(list1, list2):
+    num = min(len(list1), len(list2))
+    log.append('list1 len %d, list2 len %d, min lne %d' % (len(list1),  len(list2), num))
+    intercept_list = list1[:num]
+    intercept_list.extend(list2[:num])
+    log.append('intercept_list len %d' % len(intercept_list))
+    return intercept_list
+
+def shuffle_data(list):
+    random.shuffle(list)
+
+def devide_data(list, output_dir):
+    total_num = len(list)
+    train_num = int(total_num * PROPORTION)
+    log.append('total_num %d, train_num %d' % (total_num, train_num))
+    with open(output_dir+'train.dat', 'wb') as f:
+        for line in list[:train_num]:
+            f.write(line)
+    with open(output_dir+'test.dat', 'wb') as f:
+        for line in list[train_num:]:
+            f.write(line)
+    log.append('train file size %d' % os.path.getsize(output_dir+'train.dat'))
+    log.append('test file size %d' % os.path.getsize(output_dir+'test.dat'))
+
+if __name__ == "__main__":
+    fuzzing_bin_list = parse_raw_data_to_bin_list(FUZZING_FOLDER, '0001')
+    log.append("== deal fuzzing .. done")
+    normal_bin_list = parse_raw_data_to_bin_list(NORMAL_FOLDER, '0100')
+    log.append("== deal normal .. done")
+    intercept_bin_list = intercept_data(fuzzing_bin_list, normal_bin_list)
+    log.append("== intercept .. done")
+    shuffle_data(intercept_bin_list)
+    log.append("== shuffle .. done")
+    devide_data(intercept_bin_list, OUTPUT_FOLDER)
+    log.append("== devide .. done")
+
+    with open(LOG_FILE, 'w') as f:
+        for line in log:
+            f.write(line+'\n')
+            print(line)
--- fuzzing/mine_1/data/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -
-
-cat $1 | while read line
-do
-	if [[ $line =~ ^00 ]];then
-		echo $line
-	fi
-done
