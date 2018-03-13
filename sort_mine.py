fa85615e8cfde66609ed7a4e5a49203a5f004945
cooli7wa <liuqi@microtrust.com.cn>
2018-03-13 12:04:45 +0800

    fix sort error
    Issue No:

--- sort_mine.py
+++ sort_mine.py
@@ -6,7 +6,7 @@ import time
 # 简单选择法, O(n^2), O(1), 稳定
 def sort_number0(number):
     n = len(number)
-    for i in range(n):
+    for i in range(n-1):
         max_num = 0
         for j in range(n-i):
             if number[j] > number[max_num]:
@@ -15,7 +15,8 @@ def sort_number0(number):
     return number
 
 
-# 简单选择法（改进）, O(n^2), O(1), 稳定
+# 有问题 -> 简单选择法（改进）, O(n^2), O(1), 稳定
+# [1,2,0]结果是[2, 0, 1]
 def sort_number0_1(number):
     n = len(number)
     for i in range(n/2):
@@ -56,7 +57,7 @@ def sort_number1_1(number):
 
 time_s = time.time()
 for i in range(100000):
-    result = sort_number1_1([2,3,10,2,1,3,7,5,15,2,8,13])
+    result = sort_number0_1([1,2,0])
 print(result)
 time_e = time.time()
 print("%s" % (time_e - time_s))
